b2a1bcf83693c56c1ca4bea746cf6068dce62eee
Pankaj Kumar <osrprabhatyadav4@gmail.com>
2025-05-17 04:41:25 +0530

    Initial commit - Python Bootcamp

--- 21_PROJECTS/05_DRINK_WATER_REMINDER/main.py
+++
@@ -1,9 +0,0 @@
-import time
-from plyer import notification
-
-# Infinite loop to continuously send notifications
-while True:
-    print("Drink Water Notification Started")  # Log message to indicate notification process has started
-    # Display a notification to remind the user to drink water
-    notification.notify(title="Please drink some", message="You need to drink some water!")
-    time.sleep(5)  # Wait for 5 seconds before sending the next notification
